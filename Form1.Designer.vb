839b3c15e92f834468ba45e92312069c07159556
Walker Dick <walkerraydick2@gmail.com>
2022-11-09 13:40:06 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.LB_SongHistory = New System.Windows.Forms.ListBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BTN_TTS_TGL_ON = New System.Windows.Forms.Button()
+        Me.BTN_TTS_TGL_OFF = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'LB_SongHistory
+        '
+        Me.LB_SongHistory.FormattingEnabled = True
+        Me.LB_SongHistory.Location = New System.Drawing.Point(13, 13)
+        Me.LB_SongHistory.Name = "LB_SongHistory"
+        Me.LB_SongHistory.Size = New System.Drawing.Size(373, 394)
+        Me.LB_SongHistory.TabIndex = 0
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Spotify Now Playing"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'BTN_TTS_TGL_ON
+        '
+        Me.BTN_TTS_TGL_ON.Location = New System.Drawing.Point(433, 44)
+        Me.BTN_TTS_TGL_ON.Name = "BTN_TTS_TGL_ON"
+        Me.BTN_TTS_TGL_ON.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_TTS_TGL_ON.TabIndex = 1
+        Me.BTN_TTS_TGL_ON.Text = "ON"
+        Me.BTN_TTS_TGL_ON.UseVisualStyleBackColor = True
+        '
+        'BTN_TTS_TGL_OFF
+        '
+        Me.BTN_TTS_TGL_OFF.Enabled = False
+        Me.BTN_TTS_TGL_OFF.Location = New System.Drawing.Point(514, 44)
+        Me.BTN_TTS_TGL_OFF.Name = "BTN_TTS_TGL_OFF"
+        Me.BTN_TTS_TGL_OFF.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_TTS_TGL_OFF.TabIndex = 2
+        Me.BTN_TTS_TGL_OFF.Text = "OFF"
+        Me.BTN_TTS_TGL_OFF.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(433, 133)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(514, 133)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Button1"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(430, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "TTS Notifs"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(430, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Discord Webhooks"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(433, 177)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(344, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(433, 229)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(130, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(430, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "TTS Notifs"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(430, 200)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "TTS Notifs"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BTN_TTS_TGL_OFF)
+        Me.Controls.Add(Me.BTN_TTS_TGL_ON)
+        Me.Controls.Add(Me.LB_SongHistory)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Spotify Now Playing"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LB_SongHistory As ListBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents BTN_TTS_TGL_ON As Button
+    Friend WithEvents BTN_TTS_TGL_OFF As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,146 @@
+﻿
+
+Imports System.Speech.Synthesis
+Imports Tulpep.NotificationWindow
+Imports DiscordWebhook
+Public Class Form1
+    Public Class Settings_obj
+
+    End Class
+    Dim spotifProcess As Process()
+    Dim title_history As New List(Of String)({"---"})
+    Dim Announce_TTS As Boolean = False
+    Dim synth As New System.Speech.Synthesis.SpeechSynthesizer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        spotifProcess = Process.GetProcessesByName("spotify")
+        Get_Proc_title()
+        Timer1.Enabled = True
+
+    End Sub
+
+    Function Get_Proc_title()
+        spotifProcess = Process.GetProcessesByName("spotify")
+        For Each Pro In spotifProcess
+            If Pro.MainWindowTitle.Length > 0 Then
+                TitleChange(Pro.MainWindowTitle.ToString)
+            End If
+        Next
+        Return True
+    End Function
+    Function TitleChange(title As String)
+
+        If title_history.Count > 0 Then
+            Dim last_song = title_history.Last()
+            ' Debug.WriteLine("Last song: " + last_song)
+            If Not (title = last_song) Then
+                'Debug.WriteLine("Cur song:" + title)
+                title_history.Add(title)
+                trim_history(5)
+                Update_Hist_LB()
+
+
+                Assemble_TTS_Announcemet(title, Announce_TTS)
+            End If
+        Else
+            title_history.Add(title)
+            Update_Hist_LB()
+
+        End If
+        ' Debug.WriteLine(title_history.Count)
+        'Debug.WriteLine(title)
+        Return title
+    End Function
+
+    Function trim_history(SLen As Integer)
+        If title_history.Count > SLen Then
+            title_history.RemoveAt(0)
+        End If
+        Return True
+
+    End Function
+    Function Update_Hist_LB()
+        LB_SongHistory.Items.Clear()
+        For Each song In title_history
+            LB_SongHistory.Items.Add(song)
+        Next
+        LB_SongHistory.Refresh()
+        Return True
+    End Function
+    Function Assemble_TTS_Announcemet(Str_In As String, tts As Boolean)
+        Dim items = Str_In.Split("-")
+        Dim np_info_TTS = "Now playing " + items.Last().ToString() + " by " + items.First().ToString()
+        Dim np_info_popup = items.Last().ToString() + " by " + items.First().ToString()
+        ' Debug.WriteLine(np_info)
+        Announce(np_info_TTS, np_info_popup, tts)
+        Return True
+    End Function
+    Function Announce(announce_info_tts As String, announce_info_popup As String, tts As Boolean)
+        If tts Then
+            synth.Speak(announce_info_tts)
+        End If
+        PopUpNotif(announce_info_popup)
+        Return True
+    End Function
+    Function PopUpNotif(info As String)
+        Dim popupNotifEnt = New PopupNotifier()
+        popupNotifEnt.ContentText = info
+        popupNotifEnt.BodyColor = Color.White
+        popupNotifEnt.Delay = 5000
+        popupNotifEnt.ContentPadding = New Padding(12)
+        popupNotifEnt.TitleText = "Spotify Now Playing"
+        popupNotifEnt.Popup()
+        Return True
+    End Function
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Get_Proc_title()
+    End Sub
+    Function Update_TTS_BTNS()
+        If Announce_TTS Then
+            BTN_TTS_TGL_OFF.Enabled = True
+            BTN_TTS_TGL_ON.Enabled = False
+        Else
+            BTN_TTS_TGL_OFF.Enabled = False
+            BTN_TTS_TGL_ON.Enabled = True
+        End If
+        Return True
+    End Function
+    Function Toggle_TTS_ANN()
+        Announce_TTS = Not (Announce_TTS)
+        Update_TTS_BTNS()
+        Return True
+    End Function
+    Private Sub BTN_TTS_TGL_ON_Click(sender As Object, e As EventArgs) Handles BTN_TTS_TGL_ON.Click
+        Toggle_TTS_ANN()
+    End Sub
+
+    Private Sub BTN_TTS_TGL_OFF_Click(sender As Object, e As EventArgs) Handles BTN_TTS_TGL_OFF.Click
+        Toggle_TTS_ANN()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            'NotifyIcon1.Icon = SystemIcons.Application
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "Notice"
+            NotifyIcon1.BalloonTipText = "Double Click the taskbar Icon to show the window"
+            NotifyIcon1.ShowBalloonTip(5000)
+            'Me.Hide()
+            ShowInTaskbar = False
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        'Me.Show()
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+    Function rnd_color_hex() As String
+        Dim random_rand = New Random()
+        Dim color = String.Format("#{0:X6}", random_rand.Next(&H1000000))
+        Return color
+    End Function
+End Class
